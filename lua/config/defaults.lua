599c35af71ed41c0ca035efa6c4e0b4f283a3d51
George <george@machisoul.com>
2023-08-08 15:51:21 +0800

    update defaults.lua and keymaps.lua

--- lua/config/defaults.lua
+++ lua/config/defaults.lua
@@ -1,1 +1,9 @@
+vim.o.number = true
 vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.tabstop = 4
+vim.o.smarttab = true
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+--vim.o.list = true
+-- vim.o.listchars = 'tab:|\\ ,trail:▫'
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,22 +4,47 @@ local mode_nv = { "n", "v" }
 local mode_v = { "v" }
 local mode_i = { "i" }
 local nmappings = {
-	{ from = "S",		to = ":w<CR>" },
+	-- Basic Mappings
+	{ from = "S",	to = ":w<CR>" },
 	{ from = "Q",		to = ":q<CR>" },
 	{ from = ";",		to = ":",		mode = mode_nv },
-
-	-- Movement
-	{ from = "i",		to = "k",		mode = mode_nv },
-	{ from = "k",		to = "j",		mode = mode_nv },
-	{ from = "j",		to = "h",               mode = mode_nv },
-	{ from = "gi",		to = "gk",		mode = mode_nv },
-	{ from = "gk",		to = "gj",		mode = mode_nv },
-	{ from = "J",		to = "0",		mode = mode_nv },
-	{ from = "L",		to = "$",		mode = mode_nv },
 
 	-- Actions
 	{ from = "h",		to = "i",		mode = mode_nv },
 	{ from = "H",		to = "I",		mode = mode_nv },
+	-- copy a line
+	{ from = "yl",		to = "0v$hy",		mode = mode_nv },
+	-- don't show search result
+	{ from = "<leader><CR>",		to = ":nohlsearch<CR>",		mode = mode_nv },
+
+	-- Movement
+	-- basic movement
+	--"     ^
+	--"     i
+	--" < j   l >
+	--"     k
+	--"     v
+	{ from = "i",		to = "k",		mode = mode_nv },
+	{ from = "k",		to = "j",		mode = mode_nv },
+	{ from = "j",		to = "h",		mode = mode_nv },
+	{ from = "gi",		to = "gk",		mode = mode_nv },
+	{ from = "gk",		to = "gj",		mode = mode_nv },
+	{ from = "I",		to = "5k",		mode = mode_nv },
+	{ from = "K",		to = "5j",		mode = mode_nv },
+	{ from = "J",		to = "0",		mode = mode_nv },
+	{ from = "L",		to = "$",		mode = mode_nv },
+	-- search result movement
+	{ from = "n",		to = "nzz",		mode = mode_nv },
+	{ from = "N",		to = "Nzz",		mode = mode_nv },
+	-- view movement
+	{ from = "<c-i>",		to = "<c-y>",		mode = mode_nv },
+	{ from = "<c-k>",		to = "<c-e>",		mode = mode_nv },
+	-- jump movement
+	-- jump to the next/new position
+	{ from = "<c-p>",		to = "<c-o>",		mode = mode_nv },
+	-- jump to the pervious position
+	{ from = "<c-n>",		to = "<c-i>",		mode = mode_nv },
+
 }
 
 
@@ -27,16 +52,3 @@ for _, mapping in ipairs(nmappings) do
 	vim.keymap.set(mapping.mode or "n", mapping.from, mapping.to, { noremap = true })
 end
 
-local function run_vim_shortcut(shortcut)
-	local escaped_shortcut = vim.api.nvim_replace_termcodes(shortcut, true, false, true)
-	vim.api.nvim_feedkeys(escaped_shortcut, 'n', true)
-end
-
--- close win below
-vim.keymap.set("n", "<leader>q", function()
-	vim.cmd("TroubleClose")
-	local wins = vim.api.nvim_tabpage_list_wins(0)
-	if #wins > 1 then
-		run_vim_shortcut([[<C-w>j:q<CR>]])
-	end
-end, { noremap = true, silent = true })
