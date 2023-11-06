e203b166ddca46973e11bc293a2f334c7a4bc62f
vidge <vidge@machisoul.com>
2023-11-06 21:18:58 +0800

    add flash plugin.

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -10,3 +10,9 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+vim.keymap.set("n", "<leader>pl", ":Lazy<CR>", { noremap = true})
+
+require("lazy").setup({
+	require("config.plugins.flash"),
+})
+
