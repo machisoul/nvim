c6b511c1c4423d7910dc58efa18d94c93c21485f
vidge <vidge@machisoul.com>
2023-11-06 21:15:48 +0800

    reset keymap and default setting.

--- lua/config/defaults.lua
+++ lua/config/defaults.lua
@@ -1,6 +1,6 @@
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.cursorline = true
+vim.o.cursorline = false
 vim.o.tabstop = 4
 vim.o.smarttab = true
 vim.o.shiftwidth = 4
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,13 +6,11 @@ local mode_i = { "i" }
 local nmappings = {
 	-- Basic Mappings
 	{ from = "S",	to = ":w<CR>" },
-	{ from = "Q",		to = ":q<CR>" },
-	{ from = ";",		to = ":",		mode = mode_nv },
+	{ from = "Q",	to = ":q<CR>" },
+	{ from = ";",	to = ":",	mode = mode_nv },
 
 	-- copy a line
-	{ from = "yl",		to = "0v$hy",		mode = mode_nv },
 	-- don't show search result
-	{ from = "<leader><CR>",		to = ":nohlsearch<CR>",		mode = mode_nv },
 
 	-- Movement
 	-- basic movement
@@ -22,35 +20,33 @@ local nmappings = {
 	--"     j
 	--"     v
 	-- search result movement
-	{ from = "n",		to = "nzz",		mode = mode_nv },
-	{ from = "N",		to = "Nzz",		mode = mode_nv },
 	-- view movement
 	-- jump movement
 	-- jump to the next/new position
 	-- jump to the pervious position
 
 	-- Window & Splits
-	{ from = "<leader>k",		to = "<c-w>k", },
-	{ from = "<leader>j",		to = "<c-w>j", },
-	{ from = "<leader>h",		to = "<c-w>h", },
-	{ from = "<leader>l",		to = "<c-w>l", },
-	{ from = "s",		to = "<nop>", },
-	{ from = "sk",		to = ":set nosplitbelow<CR>:split<CR>:set splitbelow<CR>", },
-	{ from = "sj",		to = ":set splitbelow<CR>:split<CR>", },
-	{ from = "sh",		to = ":set nosplitright<CR>:vsplit<CR>:set splitright<CR>", },
-	{ from = "sl",		to = ":set splitright<CR>:vsplit<CR>", },
-	{ from = "<up>",		to = ":res +5<CR>", },
-	{ from = "<down>",		to = ":res -5<CR>", },
-	{ from = "<left>",		to = ":vertical resize-5<CR>", },
-	{ from = "<right>",		to = ":vertical resize+5<CR>", },
+	{ from = "<leader>k",	to = "<c-w>k" },
+	{ from = "<leader>j",	to = "<c-w>j" },
+	{ from = "<leader>h",	to = "<c-w>h" },
+	{ from = "<leader>l",	to = "<c-w>l" },
+	{ from = "s",	to = "<nop>" },
+	{ from = "sk",	to = ":set nosplitbelow<CR>:split<CR>:set splitbelow<CR>" },
+	{ from = "sj",	to = ":set splitbelow<CR>:split<CR>" },
+	{ from = "sh",	to = ":set nosplitright<CR>:vsplit<CR>:set splitright<CR>" },
+	{ from = "sl",	to = ":set splitright<CR>:vsplit<CR>" },
+	{ from = "<up>",	to = ":res +5<CR>" },
+	{ from = "<down>",	to = ":res -5<CR>" },
+	{ from = "<left>",	to = ":vertical resize-5<CR>" },
+	{ from = "<right>",	to = ":vertical resize+5<CR>" },
 	
 	-- Tab management
-	{ from = "ta",		to = ":tabe<CR>", },
-	{ from = "tA",		to = ":tab split<CR>", },
-	{ from = "th",		to = ":-tabnext<CR>", },
-	{ from = "tl",		to = ":+tabnext<CR>", },
-	{ from = "tmh",		to = ":-tabmove<CR>", },
-	{ from = "tml",		to = ":+tabmove<CR>", },
+	{ from = "ta",	to = ":tabe<CR>" },
+	{ from = "tA",	to = ":tab split<CR>" },
+	{ from = "th",	to = ":-tabnext<CR>" },
+	{ from = "tl",	to = ":+tabnext<CR>" },
+	{ from = "tmh",	to = ":-tabmove<CR>" },
+	{ from = "tml",	to = ":+tabmove<CR>" },
 }
 
 
