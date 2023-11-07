9c6d7b9183dad72bad281ec9041853ba7dc87fb7
vidge <vidge@machisoul.com>
2023-11-07 21:15:17 +0800

    set "gl" for "go to a line" in flash plugin.

--- lua/config/plugins/flash.lua
+++ lua/config/plugins/flash.lua
@@ -1,3 +1,9 @@
+local goLine = {
+  search = { mode = "search", max_length = 0 },
+  label = { after = { 0, 0 } },
+  pattern = "^"
+}
+
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
@@ -10,5 +16,7 @@ return {
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+	-- jump to a line.
+    { "gl", mode = { "n", "x", "o" }, function() require("flash").jump(goLine) end, desc = "Flash" },
   },
 }
